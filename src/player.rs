45c41ddb1578187d7ec8b0bbc56ca2757732457f
udon <myudongen@gmail.com>
2022-02-26 15:30:21 +0900

    use KinematicBody for player, add function to move and jump

--- src/player.rs
+++ src/player.rs
@@ -8,25 +8,26 @@ use OBSTACLE_DAMAGE;
 const DEFAULT_PLAYER_SPEED : f32 = 3.0;
 
 #[derive(NativeClass, Default)]
-#[inherit(RigidBody)]
+#[inherit(KinematicBody)]
 #[register_with(Self::register_signals)]
 pub struct Player {
     #[property(default = 3.0)]
     speed: f32,
-    #[property(default = 3.0)]
+    #[property(default = 5.0)]
     jump_power: f32,
+    #[property(default = 20.0)]
+    fall_acceleration: f32,
     
     life: i32,
     move_velocity: Vector3,
 
     is_dead: bool,
-    is_jumping: bool,
     collision_shape: Option<Ref<CollisionShape>>,
 }
 
 #[gdnative::methods]
 impl Player {
-    fn new(_owner: &RigidBody) -> Self {
+    fn new(_owner: &KinematicBody) -> Self {
         Player{
             speed: DEFAULT_PLAYER_SPEED,
             life: 0,
@@ -35,9 +36,8 @@ impl Player {
     }
     
     #[export]
-    fn _ready(&mut self, owner: &RigidBody) {
+    fn _ready(&mut self, owner: &KinematicBody) {
         self.is_dead = false;
-        self.is_jumping = false;
         self.move_velocity = Vector3::new(0., 0., self.speed);
         self.collision_shape = Some(unsafe {
             owner
@@ -48,25 +48,23 @@ impl Player {
     }
 
     #[export]
-    fn _physics_process(&mut self, owner: &RigidBody, delta: f64) {
+    fn _physics_process(&mut self, owner: &KinematicBody, delta: f64) {
         if self.is_dead == false {
+            let is_on_floor = owner.is_on_floor();
             let input = Input::godot_singleton();
-            if input.is_action_pressed("ui_left") && self.is_jumping == false {
+            if input.is_action_just_pressed("key_space") && is_on_floor {
                 self.jump();
-            }
-            if self.is_jumping && self.move_velocity.y > 0. { 
-                self.move_velocity.y -= 0.1;
-            }
-            if self.move_velocity.y < 0. {
-                self.move_velocity.y = 0.;
+                godot_print!("jump")
             }
             
-            owner.translate(self.move_velocity.mul(delta as f32));
+            self.move_velocity.z = self.speed;
+            self.move_velocity.y -= self.fall_acceleration * (delta as f32);
+            self.move_velocity = owner.move_and_slide(self.move_velocity, Vector3::new(0., 1., 0.), false, 5, 3., false);
         }
     }
     
     #[export]
-    fn on_hit_obstacle(&mut self, owner: &RigidBody) {
+    fn on_hit_obstacle(&mut self, owner: &KinematicBody) {
         self.damage(OBSTACLE_DAMAGE);
     }
 
@@ -94,12 +92,11 @@ impl Player {
     }
     
     fn jump(&mut self) {
-        if self.is_jumping || self.is_dead { 
+        if self.is_dead { 
             return;
         }
         
-        self.is_jumping = true;
-        self.move_velocity.y = 5.0;
+        self.move_velocity.y = self.jump_power;
     }
     
     fn slide(&mut self) {
