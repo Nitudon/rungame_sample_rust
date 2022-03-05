516f6bb8c72c38c8d7ecef57780f99f90da4074b
udon <myudongen@gmail.com>
2022-03-05 19:13:37 +0900

    change player speed system, from constant to acceleration

--- src/player.rs
+++ src/player.rs
@@ -1,23 +1,25 @@
 use gdnative::*;
 use gdnative::prelude::*;
 use gdnative::api::*;
-use OBSTACLE_DAMAGE;
 
 #[derive(NativeClass, Default, Debug)]
 #[inherit(KinematicBody)]
 #[register_with(Self::register_signals)]
 pub struct Player {
+    #[property(default = 2.0)]
+    horizontal_speed: f32,
     #[property(default = 3.0)]
-    speed: f32,
+    max_forward_speed: f32,
     #[property(default = 5.0)]
     jump_power: f32,
+    #[property(default = 5.0)]
+    move_acceleration: f32,
     #[property(default = 20.0)]
     fall_acceleration: f32,
-    #[property(default = 3)]
-    life: i32,
     
     move_velocity: Vector3,
     is_dead: bool,
+    is_finish: bool,
     collision_shape: Option<Ref<CollisionShape>>,
 }
 
@@ -25,6 +27,11 @@ pub struct Player {
 impl Player {
     fn new(_owner: &KinematicBody) -> Self {
         Player{
+            horizontal_speed: 2.0,
+            max_forward_speed: 3.0,
+            jump_power: 5.0,
+            move_acceleration: 5.0,
+            fall_acceleration: 20.0,
             ..Default::default()
         }
     }
@@ -32,7 +39,8 @@ impl Player {
     #[export]
     fn _ready(&mut self, owner: &KinematicBody) {
         self.is_dead = false;
-        self.move_velocity = Vector3::new(0., 0., self.speed);
+        self.is_finish = false;
+        self.move_velocity = Vector3::zero();
         self.collision_shape = Some(unsafe {
             owner
                 .get_node_as::<CollisionShape>("CollisionShape")
@@ -43,10 +51,14 @@ impl Player {
 
     #[export]
     fn _physics_process(&mut self, owner: &KinematicBody, delta: f64) {
-        if self.is_dead == false {
+        if self.is_dead == false && self.is_finish == false {
             let is_on_floor = owner.is_on_floor();
             let input = Input::godot_singleton();
-            self.move_velocity.x = 0.;
+            if self.move_velocity.z < self.max_forward_speed {
+                self.move_velocity.z += self.move_acceleration * delta as f32; 
+            } else if self.move_velocity.z > self.max_forward_speed {
+                self.move_velocity.z -= self.move_acceleration * delta as f32;
+            };
             
             if input.is_action_just_pressed("key_space") && is_on_floor {
                 self.jump();
@@ -54,14 +66,13 @@ impl Player {
             }
             
             if input.is_action_pressed("ui_left") && is_on_floor {
-                self.move_velocity.x += self.speed;
+                self.move_velocity.x += self.horizontal_speed;
             }
 
             if input.is_action_pressed("ui_right") && is_on_floor {
-                self.move_velocity.x -= self.speed;
+                self.move_velocity.x -= self.horizontal_speed;
             }
             
-            self.move_velocity.z = self.speed;
             self.move_velocity.y -= self.fall_acceleration * (delta as f32);
             self.move_velocity = owner.move_and_slide(self.move_velocity, Vector3::new(0., 1., 0.), false, 5, 3., false);
         }
@@ -69,13 +80,12 @@ impl Player {
     
     #[export]
     fn on_hit_obstacle(&mut self, _owner: &KinematicBody) {
-        self.damage(OBSTACLE_DAMAGE);
     }
 
     #[export]
     fn stop(&mut self, _owner: &KinematicBody) {
-        self.speed = 0.;
         self.move_velocity = Vector3::zero();
+        self.is_finish = true;
         
         godot_print!("stop player")
     }
@@ -92,27 +102,19 @@ impl Player {
         });
     }
     
-    fn damage(&mut self, damage: i32) {
+    pub fn accelerate(&mut self, value: f32) {
         if self.is_dead {
             return;
         }
-        
-        self.life -= damage;
-        if self.life <= 0 {
-            self.life = 0;
-            self.is_dead = true;
-        }
+
+        self.move_velocity.z += value;
     }
     
-    fn jump(&mut self) {
+    pub fn jump(&mut self) {
         if self.is_dead { 
             return;
         }
         
         self.move_velocity.y = self.jump_power;
-    }
-    
-    fn slide(&mut self) {
-        unimplemented!("プレイヤーのスライディング未実装");
     }
 }
